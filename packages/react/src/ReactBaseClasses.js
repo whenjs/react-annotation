75660ca5f3ac1be07fbd76d7e58579fdd1317ebf
Eric <fxw186132@alibaba-inc.com>
2018-11-14 10:13:33 +0800

    annotation: tr en comments to cn

--- packages/react/src/ReactBaseClasses.js
+++ packages/react/src/ReactBaseClasses.js
@@ -17,14 +17,17 @@ if (__DEV__) {
 
 /**
  * Base class helpers for the updating state of a component.
+ * 基类工具函数，用于更新一个组件的状态
  */
 function Component(props, context, updater) {
   this.props = props;
   this.context = context;
   // If a component has string refs, we will assign a different object later.
+  // 如果一个组件有一个string类型的refs，后续会对它重新赋值一个对象
   this.refs = emptyObject;
   // We initialize the default updater but the real one gets injected by the
   // renderer.
+  // 这一先初始化一个默认的updater，renderer会注入真正的updater
   this.updater = updater || ReactNoopUpdateQueue;
 }
 
@@ -33,14 +36,17 @@ Component.prototype.isReactComponent = {};
 /**
  * Sets a subset of the state. Always use this to mutate
  * state. You should treat `this.state` as immutable.
+ * 设置该组件状态的一个子集，一定要用这个函数来修改state，「this.state」应该是不变的
  *
  * There is no guarantee that `this.state` will be immediately updated, so
  * accessing `this.state` after calling this method may return the old value.
- *
+ * 在调用这个函数后，「this.state」并不一定会立即更新，所以此时访问到的值可能是旧值
+ * 
  * There is no guarantee that calls to `setState` will run synchronously,
  * as they may eventually be batched together.  You can provide an optional
  * callback that will be executed when the call to setState is actually
  * completed.
+ * 因为状态更新可能会批量进行，「setState」并不一定是同步执行，可以为「setState」提供一个可选的回调函数，这个回调函数会在「setState」实际执行完成之后立即执行。
  *
  * When a function is provided to setState, it will be called at some point in
  * the future (not synchronously). It will be called with the up to date
@@ -48,6 +54,8 @@ Component.prototype.isReactComponent = {};
  * from this.* because your function may be called after receiveProps but before
  * shouldComponentUpdate, and this new state, props, and context will not yet be
  * assigned to this.
+ * 可以为「setState」提供一个在未来某个时间点执行的函数（非同步），函数执行时会传入当前最新的组件参数（state，props，context）
+ * 因为函数的执行时机是在receiveProps之后，shouldComponentUpdate之前，这时组件地新的state，props和context还没有赋值到this，因此这些参数了能会跟this访问到的对应的数一有差异，
  *
  * @param {object|function} partialState Next partial state or function to
  *        produce next partial state to be merged with current state.
@@ -69,12 +77,15 @@ Component.prototype.setState = function(partialState, callback) {
 /**
  * Forces an update. This should only be invoked when it is known with
  * certainty that we are **not** in a DOM transaction.
+ * 强制一次更新，当确切直到组件不在DOM事物中的时候才可以调用
  *
  * You may want to call this when you know that some deeper aspect of the
  * component's state has changed but `setState` was not called.
+ * 在组件没有调用「setState」而状态某些深层次发生了变化时可能会用到
  *
  * This will not invoke `shouldComponentUpdate`, but it will invoke
  * `componentWillUpdate` and `componentDidUpdate`.
+ * 调用这个函数不会触发「shouldComponentUpdate」，但会触发「componentWillUpdate」和「componentDidUpdate」
  *
  * @param {?function} callback Called after update is complete.
  * @final
@@ -88,6 +99,8 @@ Component.prototype.forceUpdate = function(callback) {
  * Deprecated APIs. These APIs used to exist on classic React classes but since
  * we would like to deprecate them, we're not going to move them over to this
  * modern base class. Instead, we define a getter that warns if it's accessed.
+ * 要弃用的APIs，这些APIs过去存在于经典的React类中，由于将要弃用它们,并不会将他们移到新的base类中；相反，当它们被访问到的时候，
+ * 会通过getter触发一个警告
  */
 if (__DEV__) {
   const deprecatedAPIs = {
